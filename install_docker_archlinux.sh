d424722a34841b622f49a022f18f5bebe9d11ddd
Grzegorz Cichosz <32677600+Talandar99@users.noreply.github.com>
2022-12-22 19:28:09 +0100

    Update install_docker_archlinux.sh

--- install_docker_archlinux.sh
+++ install_docker_archlinux.sh
@@ -2,6 +2,6 @@
 sudo pacman -S docker
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
-USER=whoami
+USER=$(whoami)
 sudo usermod -aG docker $USER
 echo "docker installed. Please reboot your machine to apply changes"
