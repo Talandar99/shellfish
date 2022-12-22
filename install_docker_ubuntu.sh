f1ff743c2b623d881ece837311d43869251322b1
Grzegorz Cichosz <32677600+Talandar99@users.noreply.github.com>
2022-12-22 19:27:34 +0100

    whoami

--- install_docker_ubuntu.sh
+++ install_docker_ubuntu.sh
@@ -4,7 +4,7 @@ sudo curl -fsSL https://get.docker.com -o get-docker.sh
 echo "Relax now"
 echo "It may take a while..."
 sudo sh get-docker.sh
-USER=whoami
+USER=$(whoami)
 sudo usermod -aG docker $USER
 sudo rm get-docker.sh
 echo "docker installed. Please reboot your machine to apply changes"
