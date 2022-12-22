a67239211b19fc449f53bbc5addc4933ccd10753
Grzegorz Cichosz <32677600+Talandar99@users.noreply.github.com>
2022-12-22 17:30:26 +0100

    edited nvim_shrimp_config

--- get_configurations.sh
+++ get_configurations.sh
@@ -3,10 +3,10 @@ sudo pacman -S tmux rustup starship bash-language-server lua-language-server vsc
 mkdir .config
 cd ~/.config
 git clone https://github.com/Talandar99/starship_config
-git clone https://github.com/Talandar99/nvim_config
+git clone https://github.com/Talandar99/nvim_shrimp_config
 git clone https://github.com/Talandar99/tmux_config
 mv starship_config starship
-mv nvim_config nvim
+mv nvim_shrimp_config nvim
 mv tmux_config tmux
 cd ~ 
 cat ~/.config/starship/.bashrc > .bashrc  
