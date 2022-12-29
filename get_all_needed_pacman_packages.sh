e35a40e85d09a18a5a7d3cf084b142db15f23b68
Talandar99 <grzegorzc66@gmail.com>
2022-12-29 15:45:30 +0100

    removed  docker grom packaman packages

--- get_all_needed_pacman_packages.sh
+++ get_all_needed_pacman_packages.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo pacman -S neovim tmux starship bash-language-server rustup elixir vscode-html-languageserver lua-language-server postgresql git docker
+sudo pacman -S neovim tmux starship bash-language-server rustup elixir vscode-html-languageserver lua-language-server postgresql git 
