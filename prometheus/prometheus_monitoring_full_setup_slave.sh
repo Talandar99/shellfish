ade20f0fbcc161697f4a3b25a5568d34dc3c2295
talandar_virt_pc_master <none>
2023-01-05 17:26:53 +0100

    full setup for master and slave works fine

--- prometheus/prometheus_monitoring_full_setup_slave.sh
+++ prometheus/prometheus_monitoring_full_setup_slave.sh
@@ -4,17 +4,13 @@ echo 'killing all containers '
 echo '-----------------------'
 docker rm -f $(docker ps -a -q)
 echo '-----------------------'
-echo 'graphana setup and run'
-echo '-----------------------'
-./pull_and_start_graphana_container.sh
-echo '-----------------------'
 echo 'node-exporter setup and run'
 echo '-----------------------'
 ./setup_and_run_node_exporter.sh
 echo '-----------------------'
 echo 'prometheus yml and template container'
 echo '-----------------------'
-./build_yml_and_pull_prometheus_container_master.sh
+./build_yml_and_pull_prometheus_container_slave.sh
 echo '-----------------------'
 echo 'build and run prometheus container'
 echo '-----------------------'
