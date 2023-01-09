938e6610bd8379e590a77cb9745edf753fd63bfb
Grzegorz Cichosz <32677600+Talandar99@users.noreply.github.com>
2023-01-09 19:54:16 +0100

    Update flutter_setup_arch.sh

--- flutter_setup_arch.sh
+++ flutter_setup_arch.sh
@@ -2,9 +2,8 @@
 sudo pacman -S --needed git base-devel
 paru -S flutter
 sudo groupadd flutterusers
-sudo gpasswd -a talandar flutterusers
+sudo gpasswd -a $(whoami) flutterusers
 sudo chown -R :flutterusers /opt/flutter
 sudo chmod -R g+w /opt/flutter/
-sudo chown -R talandar /opt/flutter
+sudo chown -R $(whoami) /opt/flutter
 paru -S android-sdk android-sdk-platform-tools android-sdk-build-tools-latest android-platform android-studio
-
