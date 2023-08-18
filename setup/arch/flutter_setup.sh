2d0927608bf57656b9b610a0d77c480aeb0a8bc2
Talandar <talandar66@protonmail.com>
2023-08-18 20:25:20 +0200

    added web platform setup for flutter

--- setup/arch/flutter_setup.sh
+++ setup/arch/flutter_setup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-sudo pacman -Sy --needed git base-devel
+sudo pacman -Sy --needed git base-devel libtool gcc gcc-libs glibc lib32-glibc cmake ninja --noconfirm
 paru -Sy flutter --noconfirm
 sudo groupadd flutterusers
 sudo gpasswd -a $(whoami) flutterusers
 sudo chown -R :flutterusers /opt/flutter
 sudo chmod -R g+w /opt/flutter/
 sudo chown -R $(whoami) /opt/flutter
+echo "export CHROME_EXECUTABLE=brave" >> ~/.bashrc 
+echo "Added CHROME_EXECUTABLE=brave"
+echo "source ~/.bashrc to apply this change"
