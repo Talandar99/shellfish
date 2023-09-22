a88c7048b095bc9f6ae34c0674f74e2843740057
Talandar <talandar66@protonmail.com>
2023-09-22 15:16:18 +0200

    feat: get_mods.py now supports modrinth

--- docker_containers/minecraft_server/forge_1.18.2_v40.2.10/get_mods.py
+++ docker_containers/minecraft_server/forge_1.18.2_v40.2.10/get_mods.py
@@ -2,27 +2,30 @@
 
 import requests
 import os
+import shutil
 
 
-def download_file(url, save_directory):
+def download_curseforge(url, save_directory):
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.head(url, headers=headers)
     response.raise_for_status()
     redirect_url = response.headers['Location']
     redirect_url = redirect_url.split('?')[0]
-    if not redirect_url:
-        print("error no link.")
-        return
-    response = requests.get(redirect_url, headers=headers, stream=True)
+    download(redirect_url, save_directory)
+
+
+def download(url, save_directory):
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, stream=True)
     response.raise_for_status()
-    filename = os.path.basename(redirect_url)
+    filename = os.path.basename(url)
     save_path = os.path.join(save_directory, filename)
     chunks = 0
     with open(save_path, 'wb') as file:
         for chunk in response.iter_content(chunk_size=8192):
             file.write(chunk)
             chunks = chunks + 1
-            if chunks % 10 == 0:
+            if chunks % 100 == 0:
                 print(f"downloaded {chunks*8192} bytes")
     print("------------------------------------------------")
     print(f"file saved as {filename}")
@@ -30,21 +33,14 @@ def download_file(url, save_directory):
     print("------------------------------------------------")
 
 
-def display_download_links(file_path):
+def get_download_links(file_path, link_name):
     links = []
     try:
         with open(file_path, 'r') as file:
             content = file.read()
             lines = content.split("\n")
-            start_index = -1
-            for i, line in enumerate(lines):
-                if "# Mods" in line:
-                    start_index = i
-                    break
-            if start_index != -1:
-                lines = lines[start_index + 1:]
             for line in lines:
-                if "[download]" in line:
+                if link_name in line:
                     link_start = line.find("(") + 1
                     link_end = line.find(")")
                     download_link = line[link_start:link_end]
@@ -56,9 +52,20 @@ def display_download_links(file_path):
 
 if __name__ == "__main__":
     mods_file = "mods"
-    if not os.path.exists(mods_file):
-        os.makedirs(mods_file)
-
-    links = display_download_links("mods.md")
+    if os.path.exists(mods_file):
+        shutil.rmtree(mods_file)
+    os.makedirs(mods_file)
+    print("================================================")
+    print("Getting mods from curseforge")
+    print("================================================")
+    links = get_download_links("mods.md", "[download_curseforge]")
     for link in links:
-        download_file(link, mods_file)
+        download_curseforge(link, mods_file)
+
+    print("================================================")
+    print("Getting mods from modrinth")
+    print("================================================")
+    links = get_download_links("mods.md", "[download_modrinth]")
+    for link in links:
+        download(link, mods_file)
+
